fcefe14158a9ed2fec139522a81909ec8b23b85d
Muratovnik <68428072+Muratovnik@users.noreply.github.com>
2020-11-18 00:04:07 +0300

    Added scroll to top

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+var windowhight = $(window).height();
+
 $(function() {
   // menu toggle  
   $('.js-menu-toggle').on('click', function() {
@@ -6,6 +8,21 @@ $(function() {
     $('.main-header .container').toggleClass('js-bg');
   });
 });
-  
+
+
+$(window).scroll(function(){
+
+    if ($(this).scrollTop() > (windowhight - 600)) {
+        $('#scrolltotop').fadeIn();
+    } else {
+        $('#scrolltotop').fadeOut();
+    }
+
+});
+
+$('#scrolltotop').click(function(event){
+    event.preventDefault();
+    $('html').animate({scrollTop: 0}, 800)
+});
 
 
